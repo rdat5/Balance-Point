ef633b0141e36b9bba61e95c3a1ea481b4b690b7
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-03-12 00:18:39 -0800

    Add initial file

---
+++ balancepoint.py
@@ -0,0 +1,16 @@
+bl_info = {
+    "name": "Balance Point",
+    "author": "Ray Allen Datuin",
+    "version": (0, 0, 1),
+    "blender": (3, 4, 0),
+    "location": "View3D > Tools > Balance Point",
+    "description": "Visualizes the center of mass of a collection of objects",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Object",
+}
+
+import bpy
+
+if __name__ == "__main__":
+    register()
