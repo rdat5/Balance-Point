15f83288b002f575bd961febb811a7fd7ce0dafe
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-03-19 12:22:01 -0700

    Change version number

--- balancepoint.py
+++ balancepoint.py
@@ -14,7 +14,7 @@
 bl_info = {
     "name": "Balance Point",
     "author": "Ray Allen Datuin",
-    "version": (0, 9, 0),
+    "version": (1, 0, 0),
     "blender": (3, 4, 0),
     "location": "View3D > Tools > Balance Point",
     "description": "Visualizes the center of mass of collections of objects",
