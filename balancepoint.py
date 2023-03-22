e57aca7dcdb99c162a0186caf60df29e0aa6f6e7
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-03-22 14:47:17 -0700

    Clean up to be closer to blender python style guide

--- balancepoint.py
+++ balancepoint.py
@@ -11,6 +11,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from bpy.app import driver_namespace
+from bpy.app.handlers import frame_change_post
+from bpy.app.handlers import depsgraph_update_post
+from gpu_extras.batch import batch_for_shader
+from mathutils import Vector
+import bmesh
+import gpu
+import bpy
 bl_info = {
     "name": "Balance Point",
     "author": "Ray Allen Datuin",
@@ -23,14 +31,6 @@ bl_info = {
     "category": "3D View",
 }
 
-import bpy
-import gpu
-import bmesh
-from mathutils import Vector
-from gpu_extras.batch import batch_for_shader
-from bpy.app.handlers import depsgraph_update_post
-from bpy.app.handlers import frame_change_post
-from bpy.app import driver_namespace
 
 # Const
 
@@ -45,19 +45,19 @@ SHAPE_FLOOR_MARKER = [
     (-1.0, 0.0, 0.0), (1.0, 0.0, 0.0),
     (0.0, -1.0, 0.0), (0.0, 1.0, 0.0),
     (0.5, 0.0, 0), (0.4619, 0.1913, 0),
-    (0.4619, 0.1913, 0), (0.3536, 0.3536, 0), 
-    (0.3536, 0.3536, 0), (0.1913, 0.4619, 0), 
-    (0.1913, 0.4619, 0), (0.0, 0.5, 0), 
-    (0.0, 0.5, 0), (-0.1913, 0.4619, 0), 
-    (-0.1913, 0.4619, 0), (-0.3536, 0.3536, 0), 
-    (-0.3536, 0.3536, 0), (-0.4619, 0.1913, 0), 
-    (-0.4619, 0.1913, 0), (-0.5, 0.0, 0), 
-    (-0.5, 0.0, 0), (-0.4619, -0.1913, 0), 
-    (-0.4619, -0.1913, 0), (-0.3536, -0.3536, 0), 
-    (-0.3536, -0.3536, 0), (-0.1913, -0.4619, 0), 
-    (-0.1913, -0.4619, 0), (-0.0, -0.5, 0), 
-    (-0.0, -0.5, 0), (0.1913, -0.4619, 0), 
-    (0.1913, -0.4619, 0), (0.3536, -0.3536, 0), 
+    (0.4619, 0.1913, 0), (0.3536, 0.3536, 0),
+    (0.3536, 0.3536, 0), (0.1913, 0.4619, 0),
+    (0.1913, 0.4619, 0), (0.0, 0.5, 0),
+    (0.0, 0.5, 0), (-0.1913, 0.4619, 0),
+    (-0.1913, 0.4619, 0), (-0.3536, 0.3536, 0),
+    (-0.3536, 0.3536, 0), (-0.4619, 0.1913, 0),
+    (-0.4619, 0.1913, 0), (-0.5, 0.0, 0),
+    (-0.5, 0.0, 0), (-0.4619, -0.1913, 0),
+    (-0.4619, -0.1913, 0), (-0.3536, -0.3536, 0),
+    (-0.3536, -0.3536, 0), (-0.1913, -0.4619, 0),
+    (-0.1913, -0.4619, 0), (-0.0, -0.5, 0),
+    (-0.0, -0.5, 0), (0.1913, -0.4619, 0),
+    (0.1913, -0.4619, 0), (0.3536, -0.3536, 0),
     (0.3536, -0.3536, 0), (0.4619, -0.1913, 0),
     (0.4619, -0.1913, 0), (0.5, 0.0, 0)]
 
@@ -67,21 +67,22 @@ shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
 
 # Classes
 
-## Properties
 
 class MassObjectGroup(bpy.types.PropertyGroup):
-    visible : bpy.props.BoolProperty(name="Visible", default=True)
-    mass_object_collection : bpy.props.PointerProperty(name="Mass Object Collection", type=bpy.types.Collection)
-    com_floor_level : bpy.props.FloatProperty(name="Floor Level", default=0.0)
-    line_to_floor : bpy.props.BoolProperty(name="Draw Line to Floor", default=False)
-    com_location : bpy.props.FloatVectorProperty(name="Location of Center of Mass")
-    color : bpy.props.FloatVectorProperty(name="CoM Marker Color", description="Color of the CoM Marker", default=(1, 0, 1), subtype='COLOR', min=0.0, max=1.0)
-    scale : bpy.props.FloatProperty(name="CoM Marker Scale", default=0.05, description="Size of the CoM Markers (in meters)", min=0)
+    visible: bpy.props.BoolProperty(name="Visible", default=True)
+    mass_object_collection: bpy.props.PointerProperty(name="Mass Object Collection", type=bpy.types.Collection)
+    com_floor_level: bpy.props.FloatProperty(name="Floor Level", default=0.0)
+    line_to_floor: bpy.props.BoolProperty(name="Draw Line to Floor", default=False)
+    com_location: bpy.props.FloatVectorProperty(name="Location of Center of Mass")
+    color: bpy.props.FloatVectorProperty(name="CoM Marker Color", description="Color of the CoM Marker", default=(
+        1, 0, 1), subtype='COLOR', min=0.0, max=1.0)
+    scale: bpy.props.FloatProperty(name="CoM Marker Scale", default=0.05,
+                                   description="Size of the CoM Markers (in meters)", min=0)
+
 
 class ComProperties(bpy.types.PropertyGroup):
-    com_tracking_on : bpy.props.BoolProperty(name="CoM Tracking Enabled", default=True)
+    com_tracking_on: bpy.props.BoolProperty(name="CoM Tracking Enabled", default=True)
 
-## Panels
 
 class CenterOfMassPanel(bpy.types.Panel):
     """Center of mass settings"""
@@ -137,6 +138,7 @@ class CenterOfMassPanel(bpy.types.Panel):
         row.scale_y = 2.0
         row.operator("balance_point.toggle_com_update", text=update_text, icon=update_icon)
 
+
 class MassPropertiesPanel(bpy.types.Panel):
     """Mass properties panel"""
     bl_label = "Mass Properties"
@@ -144,30 +146,30 @@ class MassPropertiesPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "Balance Point"
-    
+
     def draw(self, context):
         layout = self.layout
-        selObj = context.selected_objects
-        
+        sel_obj = context.selected_objects
+
         # Mass properties
         col = layout.column(align=True)
-        
+
         row = col.row()
         row.label(text="Mass properties to selected")
 
         row = col.row(align=True)
         row.operator("balance_point.massprop_add", text="Add", icon='ADD')
         row.operator("balance_point.massprop_del", text="Remove", icon='REMOVE')
-        
+
         # Volume
         col = layout.column(align=True)
-        
+
         row = col.row()
         row.label(text="Volume")
-        
+
         row = col.row()
         row.operator("balance_point.calculate_volume", icon='CUBE')
-        
+
         # Object Origin to Center of Mass
         col = layout.column(align=True)
 
@@ -175,53 +177,53 @@ class MassPropertiesPanel(bpy.types.Panel):
         row.label(text="Set Selected Origin(s) to Object CoM")
 
         row = col.row()
-        row.operator("object.origin_set", text='Origin to Center of Mass (Volume)', icon='DOT').type='ORIGIN_CENTER_OF_VOLUME'
+        row.operator("object.origin_set", text='Origin to Center of Mass (Volume)',
+                     icon='DOT').type = 'ORIGIN_CENTER_OF_VOLUME'
 
         # Set active to selected
         col = layout.column(align=True)
-        
+
         row = col.row()
         row.label(text="Set active to selected")
-        
+
         row = col.row(align=True)
         row.operator("balance_point.set_active_true", icon='RADIOBUT_ON')
         row.operator("balance_point.set_active_false", icon='RADIOBUT_OFF')
         col.operator("balance_point.toggle_active", icon='ARROW_LEFTRIGHT')
-        
+
         # Selected mass objects
         col = layout.column(align=True)
-        
+
         row = col.row(align=True)
         row.label(text="Selected Mass Objects: ")
-        for obj in selObj:
+        for obj in sel_obj:
             if obj.get('volume') is not None:
                 box = col.box()
-                
+
                 # split box
                 split = box.split(factor=0.4)
-                
+
                 # left column
                 col1 = split.column(align=True)
                 col1.label(text="" + obj.name)
-                
+
                 # get if active
                 if obj.get("active") == True:
                     col1.label(text="Active", icon='RADIOBUT_ON')
                 elif obj.get("active") == False:
                     col1.label(text="Inactive", icon='RADIOBUT_OFF')
-                
+
                 # right column
                 obj_mass = round(obj.get("density") * obj.get("volume") * obj.get("active"), 3)
-                
+
                 col2 = split.column(align=True)
                 col2.prop(obj, '["density"]')
                 col2.prop(obj, '["volume"]')
-                col2.label(text="Mass: "  + str(obj_mass))
-        if len(selObj) > 0:
+                col2.label(text="Mass: " + str(obj_mass))
+        if len(sel_obj) > 0:
             row = layout.row()
-            row.label(text="Total Mass of Selected: " + str(round(get_total_mass(selObj), 3)))
+            row.label(text="Total Mass of Selected: " + str(round(get_total_mass(sel_obj), 3)))
 
-## Operators
 
 class AddMassObjectGroup(bpy.types.Operator):
     """Adds a new Mass Object Group"""
@@ -231,6 +233,7 @@ class AddMassObjectGroup(bpy.types.Operator):
     def execute(self, context):
         bpy.context.scene.bp_mass_object_groups.add()
         return {'FINISHED'}
+
 
 class RemoveMassObjectGroup(bpy.types.Operator):
     """Adds a new Mass Object Group"""
@@ -242,15 +245,16 @@ class RemoveMassObjectGroup(bpy.types.Operator):
             bpy.context.scene.bp_mass_object_groups.remove(len(bpy.context.scene.bp_mass_object_groups) - 1)
         return {'FINISHED'}
 
+
 class AddMassProps(bpy.types.Operator):
     """Add mass properties to selected objects"""
     bl_idname = "balance_point.massprop_add"
     bl_label = "Add mass properties to selected"
-    
+
     def execute(self, context):
-        selObj = context.selected_objects
-        
-        for obj in selObj:
+        sel_obj = context.selected_objects
+
+        for obj in sel_obj:
             if obj.type == 'MESH':
                 if obj.get('active') is None:
                     obj["active"] = True
@@ -258,17 +262,18 @@ class AddMassProps(bpy.types.Operator):
                     obj["density"] = 1.0
                 if obj.get('volume') is None:
                     obj["volume"] = 1.0
-        return {'FINISHED'} 
+        return {'FINISHED'}
+
 
 class RemoveMassProps(bpy.types.Operator):
     """Remove mass properties from selected objects"""
     bl_idname = "balance_point.massprop_del"
     bl_label = "Remove mass properties from selected"
-    
+
     def execute(self, context):
-        selObj = context.selected_objects
-        
-        for obj in selObj:
+        sel_obj = context.selected_objects
+
+        for obj in sel_obj:
             if obj.type == 'MESH':
                 if obj.get('active') is not None:
                     del obj["active"]
@@ -276,59 +281,64 @@ class RemoveMassProps(bpy.types.Operator):
                     del obj["density"]
                 if obj.get('volume') is not None:
                     del obj["volume"]
-        return {'FINISHED'} 
+        return {'FINISHED'}
+
 
 class ToggleActiveProperty(bpy.types.Operator):
     """Toggles the active property"""
     bl_idname = "balance_point.toggle_active"
     bl_label = "Toggle Active"
-    
+
     def execute(self, context):
-        selObj = context.selected_objects
-        
-        for obj in selObj:
+        sel_obj = context.selected_objects
+
+        for obj in sel_obj:
             if obj.get('active') is not None:
                 set_active(obj, (not obj['active']))
-        return {'FINISHED'} 
+        return {'FINISHED'}
+
 
 class SetActiveTrue(bpy.types.Operator):
     """Toggles the active property"""
     bl_idname = "balance_point.set_active_true"
     bl_label = "Active"
-    
+
     def execute(self, context):
-        selObj = context.selected_objects
-        
-        for obj in selObj:
+        sel_obj = context.selected_objects
+
+        for obj in sel_obj:
             if obj.get('active') is not None:
                 set_active(obj, True)
-        return {'FINISHED'} 
+        return {'FINISHED'}
+
 
 class SetActiveFalse(bpy.types.Operator):
     """Toggles the active property"""
     bl_idname = "balance_point.set_active_false"
     bl_label = "Inactive"
-    
+
     def execute(self, context):
-        selObj = context.selected_objects
-        
-        for obj in selObj:
+        sel_obj = context.selected_objects
+
+        for obj in sel_obj:
             if obj.get('active') is not None:
                 set_active(obj, False)
-        return {'FINISHED'} 
+        return {'FINISHED'}
+
 
 class CalculateVolume(bpy.types.Operator):
     """Calculate volume of selected"""
     bl_idname = "balance_point.calculate_volume"
     bl_label = "Calculate volume of selected"
-    
+
     def execute(self, context):
-        selObj = context.selected_objects
-        
-        for obj in selObj:
+        sel_obj = context.selected_objects
+
+        for obj in sel_obj:
             if obj.get('volume') is not None and obj.type == 'MESH':
                 obj['volume'] = get_volume(obj)
-        return {'FINISHED'} 
+        return {'FINISHED'}
+
 
 class ToggleCOMUpdate(bpy.types.Operator):
     """Adds/Removes center of mass render function from draw handler"""
@@ -340,7 +350,8 @@ class ToggleCOMUpdate(bpy.types.Operator):
     def execute(self, context):
         if ToggleCOMUpdate._handle is None:
             # add the draw handler
-            ToggleCOMUpdate._handle = bpy.types.SpaceView3D.draw_handler_add(draw_bp, (None, None), 'WINDOW', 'POST_VIEW')
+            ToggleCOMUpdate._handle = bpy.types.SpaceView3D.draw_handler_add(
+                draw_bp, (None, None), 'WINDOW', 'POST_VIEW')
         else:
             # remove draw handler
             bpy.types.SpaceView3D.draw_handler_remove(ToggleCOMUpdate._handle, 'WINDOW')
@@ -350,7 +361,6 @@ class ToggleCOMUpdate(bpy.types.Operator):
         bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
         return {'FINISHED'}
 
-# Function Definitions
 
 def set_active(obj, act):
     obj['active'] = act
@@ -359,9 +369,10 @@ def set_active(obj, act):
     elif act == False:
         obj.display_type = 'WIRE'
 
+
 def get_volume(obj):
     volume = 0.0
-    
+
     depsgraph = bpy.context.evaluated_depsgraph_get()
     obj_eval = obj.evaluated_get(depsgraph)
     me = obj_eval.to_mesh()
@@ -370,8 +381,9 @@ def get_volume(obj):
     obj_eval.to_mesh_clear()
     volume = bm.calc_volume()
     bm.free()
-    
+
     return volume
+
 
 def get_com(coll):
     center_of_mass = Vector((0, 0, 0))
@@ -385,11 +397,12 @@ def get_com(coll):
 
             total_mass += obj_mass
             weighted_sum += (obj_mass * obj.matrix_world.translation)
-    
+
     if total_mass > 0:
         center_of_mass = weighted_sum / total_mass
 
     return center_of_mass
+
 
 def transform_indices(vertices, scale, translate_vector):
     new_vertices = []
@@ -398,8 +411,9 @@ def transform_indices(vertices, scale, translate_vector):
             (v[0] * scale) + translate_vector.x,
             (v[1] * scale) + translate_vector.y,
             (v[2] * scale) + translate_vector.z
-            ))
+        ))
     return new_vertices
+
 
 def get_final_com_shape(group):
     group_com_loc = Vector((group.com_location[0], group.com_location[1], group.com_location[2]))
@@ -411,8 +425,8 @@ def get_final_com_shape(group):
         final_shape += (group_com_loc.to_tuple(), group_com_floor_loc.to_tuple())
     return final_shape
 
+
 def draw_bp(self, context):
-    com_props = bpy.context.scene.com_properties
     bp_mass_groups = bpy.context.scene.bp_mass_object_groups
 
     # Go through each collection, create a batch, render it
@@ -424,6 +438,7 @@ def draw_bp(self, context):
             batch = batch_for_shader(shader, 'LINES', {"pos": get_final_com_shape(group)})
             batch.draw(shader)
 
+
 def get_total_mass(objects):
     total_mass = 0
 
@@ -432,12 +447,14 @@ def get_total_mass(objects):
             obj_mass = obj.get("density") * obj.get("volume")
 
             total_mass += obj_mass
-    
+
     return total_mass
+
 
 def initialize_bp_mass_groups():
     bpy.context.scene.bp_mass_object_groups.clear()
     bpy.context.scene.bp_mass_object_groups.add()
+
 
 def update_mass_group_com(scene):
     com_props = bpy.context.scene.com_properties
@@ -450,6 +467,7 @@ def update_mass_group_com(scene):
                 mass_group.com_location = [mgc.x, mgc.y, mgc.z]
 
 # Class Registration
+
 
 classes = (
     MassObjectGroup,
@@ -465,7 +483,8 @@ classes = (
     SetActiveFalse,
     CalculateVolume,
     ToggleCOMUpdate
-    )
+)
+
 
 def register():
     for cls in classes:
@@ -481,13 +500,14 @@ def register():
         frame_change_post.append(update_mass_group_com)
         driver_namespace[HANDLER_KEY] = update_mass_group_com
 
+
 def unregister():
     if ToggleCOMUpdate._handle is not None:
         bpy.types.SpaceView3D.draw_handler_remove(ToggleCOMUpdate._handle, 'WINDOW')
 
     for cls in classes:
         bpy.utils.unregister_class(cls)
-    
+
     del bpy.types.Scene.com_properties
     del bpy.types.Scene.bp_mass_object_groups
 
@@ -496,6 +516,7 @@ def unregister():
         if driver_namespace[HANDLER_KEY] in depsgraph_update_post:
             depsgraph_update_post.remove(driver_namespace[HANDLER_KEY])
             frame_change_post.remove(driver_namespace[HANDLER_KEY])
+
 
 if __name__ == "__main__":
     register()
