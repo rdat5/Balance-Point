eb564dda4f1bf67eb8f88feb3d442f4bad705eda
rdat5 <42378063+rdat5@users.noreply.github.com>
2023-03-23 03:56:40 -0700

    Refactor to separate draw functions

--- __init__.py
+++ __init__.py
@@ -28,24 +28,20 @@ from bpy.app.handlers import persistent
 from bpy.app import driver_namespace
 from bpy.app.handlers import frame_change_post
 from bpy.app.handlers import depsgraph_update_post
-from gpu_extras.batch import batch_for_shader
 from mathutils import Vector
-import gpu
 import bpy
 
-from .shapes import *
 from .props import *
 from .ui import BalancePointPanel, MassPropertiesPanel
 from .mass_ops import AddMassObjectGroup, RemoveMassObjectGroup, AddMassProps, RemoveMassProps, ToggleActiveProperty, ToggleActiveProperty, SetActiveTrue, SetActiveFalse, CalculateVolume
 from .bp_ops import ToggleDrawing
+from .draw import draw_bp
 
 # Const
 
 HANDLER_KEY = "BP_UPDATE_FN"
 
 # Shader setup
-
-shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
 
 # Classes
 
@@ -67,42 +63,6 @@ def get_com(coll):
         center_of_mass = weighted_sum / total_mass
 
     return center_of_mass
-
-
-def transform_indices(vertices, scale, translate_vector):
-    new_vertices = []
-    for v in vertices:
-        new_vertices.append((
-            (v[0] * scale) + translate_vector.x,
-            (v[1] * scale) + translate_vector.y,
-            (v[2] * scale) + translate_vector.z
-        ))
-    return new_vertices
-
-
-def get_final_com_shape(group):
-    group_com_loc = Vector((group.com_location[0], group.com_location[1], group.com_location[2]))
-    group_com_floor_loc = Vector((group_com_loc.x, group_com_loc.y, group.com_floor_level))
-    com_shape = transform_indices(SHAPE_COM_MARKER, group.scale, group_com_loc)
-    com_floor_shape = transform_indices(SHAPE_FLOOR_MARKER, group.scale, group_com_floor_loc)
-    final_shape = com_shape + com_floor_shape
-    if group.line_to_floor:
-        final_shape += (group_com_loc.to_tuple(), group_com_floor_loc.to_tuple())
-    return final_shape
-
-
-def draw_bp(self, context):
-    com_props = bpy.context.scene.com_properties
-    bp_mass_groups = bpy.context.scene.bp_mass_object_groups
-    # Go through each collection, create a batch, render it
-    if com_props.com_drawing_on:
-        for group in bp_mass_groups:
-            if group.visible:
-                # Get color
-                shader.uniform_float("color", (group.color.r, group.color.g, group.color.b, 1.0))
-                # Get shape vertices
-                batch = batch_for_shader(shader, 'LINES', {"pos": get_final_com_shape(group)})
-                batch.draw(shader)
 
 
 def initialize_bp_mass_groups():
---
+++ draw.py
@@ -0,0 +1,43 @@
+import bpy
+import gpu
+from gpu_extras.batch import batch_for_shader
+from mathutils import Vector
+
+from .shapes import *
+
+shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+
+def draw_bp(self, context):
+    com_props = bpy.context.scene.com_properties
+    bp_mass_groups = bpy.context.scene.bp_mass_object_groups
+    # Go through each collection, create a batch, render it
+    if com_props.com_drawing_on:
+        for group in bp_mass_groups:
+            if group.visible:
+                # Get color
+                shader.uniform_float("color", (group.color.r, group.color.g, group.color.b, 1.0))
+                # Get shape vertices
+                batch = batch_for_shader(shader, 'LINES', {"pos": get_final_com_shape(group)})
+                batch.draw(shader)
+
+
+def get_final_com_shape(group):
+    group_com_loc = Vector((group.com_location[0], group.com_location[1], group.com_location[2]))
+    group_com_floor_loc = Vector((group_com_loc.x, group_com_loc.y, group.com_floor_level))
+    com_shape = transform_indices(SHAPE_COM_MARKER, group.scale, group_com_loc)
+    com_floor_shape = transform_indices(SHAPE_FLOOR_MARKER, group.scale, group_com_floor_loc)
+    final_shape = com_shape + com_floor_shape
+    if group.line_to_floor:
+        final_shape += (group_com_loc.to_tuple(), group_com_floor_loc.to_tuple())
+    return final_shape
+
+
+def transform_indices(vertices, scale, translate_vector):
+    new_vertices = []
+    for v in vertices:
+        new_vertices.append((
+            (v[0] * scale) + translate_vector.x,
+            (v[1] * scale) + translate_vector.y,
+            (v[2] * scale) + translate_vector.z
+        ))
+    return new_vertices
